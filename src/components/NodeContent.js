2250b5ac4710cf228b7ca6a69f10001da54bbf5a
eric-yates <eric@medleyagency.com>
2022-12-14 15:30:31 -0800

    Update copy

--- src/components/NodeContent.js
+++ src/components/NodeContent.js
@@ -5,14 +5,15 @@ function NodeContent(props) {
 
   return (
     <>
-      <div className='sidebar-header'>🧠 {props.header}</div>
+    <div className='content-header'>🧠 Neural Region</div>
+    <div className='content-body'>{props.header}</div>
       <div className='sidebar-body'>
         <SidebarSection title='Location' description={props.content.location || 'TBD' } />
         <SidebarSection title='Physiology' description={props.content.physiology || 'TBD' } />
         <SidebarSection title='Cytoarchitecture' description={props.content.cytoarchitecture || 'TBD' } />
-        <SidebarSection title='Gene expression' description={props.content.geneExpression || 'TBD' } />
-        <SidebarSection title='Receptor expression' description={props.content.receptorExpression || 'TBD' } />
-        <SidebarSection title='Neurotransmitter expression' description={props.neurotransmitterExpression || 'TBD' } />
+        <SidebarSection title='Gene Expression' description={props.content.geneExpression || 'TBD' } />
+        <SidebarSection title='Receptor Expression' description={props.content.receptorExpression || 'TBD' } />
+        <SidebarSection title='Neurotransmitter Expression' description={props.neurotransmitterExpression || 'TBD' } />
       </div>
     </>
   );
