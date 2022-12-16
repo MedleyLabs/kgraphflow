7dbefe2c3f2a9129640ec9561818a594b2e9f33c
eric-yates <eric@medleyagency.com>
2022-12-15 22:14:32 -0800

    Fix formatting

--- src/components/SidebarSection.js
+++ src/components/SidebarSection.js
@@ -5,7 +5,9 @@ function SidebarSection(props) {
   return (
     <div className='sidebar-section'>
       <div className='section-title'>{props.title}</div>
-      <div className='section-description' dangerouslySetInnerHTML={{__html: props.description.replaceAll('\\n\\n', '<br/><br/>')}} />
+      <div className='section-description' dangerouslySetInnerHTML={
+          {__html: props.description.replaceAll('\\n', '<br/>')}
+      }/>
     </div>
   );
 };
