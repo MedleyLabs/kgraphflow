c0d6f4d0258933fc357f7bd521892eb9d255cc71
eric-yates <eric@medleyagency.com>
2023-04-13 19:32:50 -0700

    Add arterial supply to sidebar

--- src/components/custom/NeuralRegionContent.js
+++ src/components/custom/NeuralRegionContent.js
@@ -40,14 +40,14 @@ function NeuralRegionContent(props) {
 
     return (
         <>
-            <div className='content-header'>🧠 Neural Region</div>
+            <div className='content-header'>{props.header.toLowerCase().includes('artery') ? '🫀 Arterial Supply' : '🧠 Neural Region'}</div>
             <div className='content-body'>{props.header || 'TBD'}</div>
             <div className='sidebar-body'>
                 <SidebarSection title='Parents' description={props.content.parents.length > 0 ? mapToLinks(props.content.parents) : 'None specified'} defaultIsOpen={true}/>
                 <SidebarSection title='Children' description={props.content.children.length > 0 ? mapToLinks(props.content.children): 'None specified'} defaultIsOpen={true}/>
-                <SidebarSection title='Arterial Supply' description={props.content.arterialSupply.length > 0 ? mapToLinks(props.content.arterialSupply) : 'None specified'} defaultIsOpen={true}/>
-                {/*<SidebarSection title='⌖ Location' description={props.content && props.content.location || 'TBD' } />*/}
+                <SidebarSection title={props.header.toLowerCase().includes('artery') ? 'Neural Regions' : 'Arterial Supply'} description={props.content.arterialSupply.length > 0 ? mapToLinks(props.content.arterialSupply) : 'None specified'} defaultIsOpen={true}/>
                 <SidebarSection title='Cell Types' description={props.header === 'Primary motor cortex' ? props.content.cellTypes : null} defaultIsOpen={true} style={props.header === 'Primary motor cortex' ? {height: 420} : {}}/>
+                {/*<SidebarSection title='⌖ Location' description={props.content && props.content.location || 'TBD' } />*/}
                 {/*<SidebarSection title='Gene Expression' description={props.content && props.content.geneExpression || 'TBD'} defaultIsOpen={true}/>*/}
                 {/*<SidebarSection title='Receptor Expression' description={props.content && props.content.receptorExpression || 'TBD'} defaultIsOpen={true}/>*/}
                 {/*<SidebarSection title='Neurotransmitter Expression' description={props.content && props.neurotransmitterExpression || 'TBD'} defaultIsOpen={true}/>*/}
