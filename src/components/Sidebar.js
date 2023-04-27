3eee16dd4606fa85243c6f6a891b2e553962a487
eric-yates <eric@medleyagency.com>
2023-04-26 23:48:55 -0700

    Style updates

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,29 +2,26 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
-  border-right: 1px solid #eee;
+  border-right: 1px solid lightgray;
   padding: 20px 0 20px 30px;
-  font-size: 16px;
   background: #fcfcfc;
   width: 35%;
-  height: 100%;
   font-family: 'Nunito', sans-serif;
 `
 
 const Scrollable = styled.aside`
   height: 100%;
   overflow-y: scroll;
-  padding-right: 30px;
+  padding-right: 30px; // Positions the scrollbar to the right edge of Container
 `
 
-function Sidebar(props) {
-
+const Sidebar = (props) => {
     return (
         <Container>
             {props.header}
             <Scrollable>{props.content}</Scrollable>
         </Container>
     );
-}
+};
 
 export default Sidebar;
--- src/components/SidebarSection.js
+++ src/components/SidebarSection.js
@@ -8,9 +8,10 @@ import TriangleRightIcon from './svg/TriangleRightIcon.js';
 const Section = styled.div`
   display: flex;
   flex-direction: column;
-  margin-bottom: 21px;
   border-bottom: 1px solid lightgray;
-  padding-bottom: 20px;
+  height: 100%;
+  box-sizing: border-box;
+  padding: 18px 0 19px 0;
 `
 
 const SectionHeader = styled.div`
@@ -19,7 +20,7 @@ const SectionHeader = styled.div`
 `
 
 const SectionColumn = styled.div`
-  flex: 1;
+  flex: 0 0 auto;
 `
 
 const SectionTitle = styled.span`
@@ -31,6 +32,7 @@ const SectionTitle = styled.span`
 const SectionBody = styled.div`
   font-family: 'Nunito', sans-serif;
   font-size: 0.95em;
+  padding: 18px 0 0 20px;
 `
 
 
@@ -52,31 +54,28 @@ function SidebarSection(props) {
     }
 
     return (
-        <Section style={isOpen ? props.style : {}}>
+        <Section style={props.style}>
             <SectionHeader>
-                <SectionColumn style={{flex: "0 0 auto"}}>
+                <SectionColumn>
                     {isOpen
                         ? <TriangleDownIcon styles={toggleStyles} onClick={toggleCallback}/>
                         : <TriangleRightIcon styles={toggleStyles} onClick={toggleCallback}/>
                     }
                 </SectionColumn>
-                <SectionColumn style={{}}>
+                <SectionColumn>
                     <SectionTitle>{props.title}</SectionTitle>
                     {!isOpen && props.description !== 'TBD' && props.description !== 'None specified' && !props.suppressIcon
                         ? <InfoAvailableIcon style={{transform: "translate(0, 1px)"}}/>
                         : null
                     }
-                    <SectionBody style={isOpen ? {marginTop: '1em'} : {}}>
-                        {isOpen
-                            ? typeof(props.description) === "string"
-                                ? <div dangerouslySetInnerHTML={{__html: props.description.replaceAll("\\n", "<br/>")}}/>
-                                : <div>{props.description}</div>
-                            : null
-                        }
-                    </SectionBody>
-                    {props.footer}
                 </SectionColumn>
             </SectionHeader>
+            {isOpen
+                ? typeof(props.description) === "string"
+                    ? <SectionBody dangerouslySetInnerHTML={{__html: props.description.replaceAll("\\n", "<br/>")}}/>
+                    : <SectionBody>{props.description}</SectionBody>
+                : null
+            }
         </Section>
     );
 }
